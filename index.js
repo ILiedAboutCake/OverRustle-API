c504160d499d2c4832638f5b92343f5154c1ac7d
Hayk Saakian <fakest.id.ever@gmail.com>
2014-11-14 13:45:57 -0800

    move client code to api

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function isGood(s){
   if(path.search(REGEX) > -1){
     return false
   }
-  return "/"+path
+  return "/"+path.toLowerCase()
 }
 
 var strims = {}
@@ -70,6 +70,10 @@ app.get('/api', function(req, res){
   res.set("Connection", "close");
   res.send(getStrims());
   res.end()
+});
+
+app.get('/strims.js', function(req, res){
+  res.sendFile(__dirname + '/strims.js');
 });
 
 // for debug to serve different urls
