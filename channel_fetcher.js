21fb5bad7510748eec17913c49d49b7878273608
Hayk Saakian <fakest.id.ever@gmail.com>
2014-12-10 22:24:09 -0800

    actually hgetall

--- channel_fetcher.js
+++ channel_fetcher.js
@@ -5,15 +5,18 @@ var client = require('redis').createClient(6379, redis_host, {})
 // TODO: cache
 
 module.exports = function (channel_name, callback) {
-  client.select(redis_db, function (e, obj) {
-    // return bad things
-    // if no channel
-    if(!obj || e)
-      return
-    
-    callback({
-      platform: obj.service,
-      channel: obj.stream
+  client.select(redis_db, function () {
+    client.hgetall("channel:"+channel_name, function (e, obj) {
+      // return bad things
+      // if no channel
+      if(!obj || e)
+        console.log('nothing in redis SoSad')
+        return
+      
+      callback({
+        platform: obj.service,
+        channel: obj.stream
+      })
     })
   })
 }
