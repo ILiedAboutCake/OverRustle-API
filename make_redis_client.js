69305dd1ac8d75afba4418bb42770abc637fddc2
Hayk Saakian <fakest.id.ever@gmail.com>
2015-05-25 23:07:24 -0700

    refactor redis initialization

---
+++ make_redis_client.js
@@ -0,0 +1,26 @@
+var redis_host = '127.0.0.1'
+var redis = require('redis')
+
+function makeRedisClient (redis_db) {
+  var rv = null
+  if(process.env.REDISTOGO_URL){
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    rv = redis.createClient(rtg.port, rtg.hostname);
+    rv.auth(rtg.auth.split(":")[1]);
+  }else{
+    rv = redis.createClient('6379', redis_host);
+  }
+  rv.select(redis_db); 
+
+  rv.on('connect', function() {
+    console.log('Connected to redis#'+redis_db.toString());
+  });
+  rv.on('error', function (er) {
+    console.trace('Channel_fetcher') // [1]
+    console.error(er.stack) // [2]
+  })
+
+  return rv
+}
+
+module.exports = makeRedisClient
